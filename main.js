b26d9e8de2cc909f05f5ccbae1ccadbe17eff955
Junaid Tahir <159828160+JUN91D@users.noreply.github.com>
2024-03-05 23:56:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//? 1. Install [Node.js](https://nodejs.org/en/), [TypeScript](https://www.typescriptlang.org/download)  and [VS Code](https://code.visualstudio.com/) on your computer.
+//*Done  
+//! 2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var myName = "Junaid Tahir Sardar";
+var message = "Hello, ".concat(myName, " Would you like to learn Typecript(Statical typing)");
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,14 @@
+//^Assalam o Alaikum 
+//* Name :Junaid Tahir 
+
+//? 1. Install [Node.js], [TypeScript] and [VS Code] on your computer.
+//*Done  
+
+//! 2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+const myName: string = "Junaid Tahir Sardar";
+let message: string = `Hello, ${myName} Would you like to learn Typecript(Statical typing)`
+console.log(message)
+//*Done
+
+//todo 3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let fullName: string = "JuNAid TaHIr"
